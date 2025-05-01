c308cbf9f67234bba9305aab73777346c067e157
Victor Leonov <leonovvk98@gmail.com>
2025-05-02 00:09:05 +0300

    изменил конфиг для интеграции с джирой

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,11 @@
-profile=$1
+TAG=$1
 
-yarn cucumber:localhost --profile $profile
+yarn precucumber
+
+if [ -z "$TAG" ]; then
+  echo "🟢 Запуск всех тестов"
+  yarn cucumber:localhost --profile dev
+else
+  echo "🟢 Запуск тестов с тегом: $TAG"
+  yarn cucumber:localhost --tags "$TAG"
+fi
