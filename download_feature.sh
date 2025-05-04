fab30ea9730b70c72f7e456e5897b0fda0842981
Viktor Smolov <viktorsmolov916@gmail.com>
2025-05-04 16:54:11 +0000

    дебаг

--- download_feature.sh
+++ download_feature.sh
@@ -23,8 +23,8 @@ get_test_script() {
 local trace_file="curl_trace_${test_key}.log"
 
   echo "DEBUG: Запуск get_test_script для test_key=$test_key" >&2
-    echo "▶ DEBUG (od -c) test_key:"
-    echo "$test_key" | od -c
+  echo "▶ DEBUG (od -c) test_key:" >&2
+  echo "$test_key" | od -c >&2
   for attempt in $(seq 1 $retries); do
     echo "🔄 Попытка $attempt получить $test_key..." >&2
 
