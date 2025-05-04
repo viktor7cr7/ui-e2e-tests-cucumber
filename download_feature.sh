db6772f925ccf9c8f7d13dbe1bdee5c436c1f484
Viktor Smolov <viktorsmolov916@gmail.com>
2025-05-04 16:36:39 +0000

    дебаг

--- download_feature.sh
+++ download_feature.sh
@@ -23,11 +23,11 @@ get_test_script() {
 local trace_file="curl_trace_${test_key}.log"
 
   echo "DEBUG: Запуск get_test_script для test_key=$test_key" >&2
-
+    
   for attempt in $(seq 1 $retries); do
     echo "🔄 Попытка $attempt получить $test_key..." >&2
     echo "DEBUG test_key='$test_key'" | cat -v
-
+     sleep delay
     # Получаем ответ + HTTP статус (в одну строку)
     raw_response=$(curl -s --trace-ascii "$trace_file" -w "%{http_code}" -X GET \
       "https://eu.api.zephyrscale.smartbear.com/v2/testcases/${test_key}/testscript" \
